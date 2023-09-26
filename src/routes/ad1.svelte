6557f78f4650a5822db0af20b9fb7d504dad5fd1
Josh <joshrl@proton.me>
2023-09-26 16:53:04 +0100

    .

--- src/routes/ad1.svelte
+++ src/routes/ad1.svelte
@@ -1,6 +1,9 @@
-<div>
+<div
+	class="bg-gray-200 dark:bg-gray-800 w-fit p-2 rounded-lg h-3/12 fixed bottom-0 m-3 prose dark:prose-invert"
+>
+	<h4 class="text-center">Featured video!</h4>
 	<iframe
-		class="w-4/12 h-3/12 fixed bottom-0 m-3"
+		class="rounded"
 		src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?si=HIcJuU5dOLoX0ri4"
 		title="Featured video"
 		frameborder="0"
